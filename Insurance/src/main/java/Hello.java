80422d982e73e6817b03cd1d846b6624d570415e
Pooja Singh <ab@cd.com>
2021-04-07 10:16:16 +0530

    Modified main() method and removed un-wanted print statements

--- Insurance/src/main/java/Hello.java
+++ Insurance/src/main/java/Hello.java
@@ -4,11 +4,7 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("hello there bla bla");
-		System.out.println("Hello Suar Log..");
-		System.out.println("Poooooojaaaa");
-		System.out.println("Hii Kukkr log..");
-		System.out.println("Heyyy yaaaaa...........");
+		System.out.println("hello Insurance Project");
 	}
 
 }
