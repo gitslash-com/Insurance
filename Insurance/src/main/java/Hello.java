de7b93cc80324d2d60509db69016ae985c3abb53
Rutik <Rutik@DESKTOP-4CNS1VV>
2021-04-07 11:47:39 +0530

    Final

--- Insurance/src/main/java/Hello.java
+++ Insurance/src/main/java/Hello.java
@@ -5,9 +5,7 @@ public class Hello {
 		// TODO Auto-generated method stub
 
 		System.out.println("hello Insurance Project");
-		System.out.println("Hello Test 2");
-		System.out.println("Hello Test 3");
-		System.out.println("Hello Test 4");
+		System.out.println("Hello 4");
 	}
 
 }
